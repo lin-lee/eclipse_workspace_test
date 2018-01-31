9e765a1868b469ebf6cfb97de3c90f49dd2acdc2
lilin <lilin@lvmama.com>
2018-01-31 10:13:06 +0800

    workspace_netty

---
+++ workspace_netty/netty-test/src/netty/timeserver/client/a.java
@@ -0,0 +1,11 @@
+package netty.timeserver.client;
+
+public class a {
+	
+	public static void main(String [] args){
+		Long sendedNum = 6L;
+		boolean s = (sendedNum % 6 == 0);
+		System.out.println(s);
+	}
+
+}
