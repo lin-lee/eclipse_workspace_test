a4609373ada705f6ffcc8fd0f411124d644acfaa
lilin <lilin@lvmama.com>
2018-01-31 10:52:41 +0800

    workspace_springBoot commit

---
+++ workspace_springBoot/springBoot/src/test/java/controller/SimpleController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * Created by wenchao.ren on 2014/4/26.
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class SimpleController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@ResponseBody
+	public String hello() {
+
+		return "hello world";
+
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SimpleController.class, args);
+
+	}
+
+}
